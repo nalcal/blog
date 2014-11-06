c688ed0f548fb687fc8b4a664d6f034cf2fba3a8
nalitha liyanage <nalitha770@gmail.com>
2014-11-06 11:22:32 -0800

    php videos

--- view/header.php
+++ view/header.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html> 
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 <html>
+    
     <head>
         <meta charset="UTF-8">
-        
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <title>Blogs</title>
         

9cd24f502f36b3bc6fd044f3189c2b2c65870649
nalitha liyanage <nalitha770@gmail.com>
2014-11-17 11:47:21 -0800

    php videos

--- model/Database.php
+++ model/Database.php
@@ -9,4 +9,13 @@ class Database {
     private $username;
     private $password;
     private $database;
+   //created a public function with $host, $username, $password, $database
+    public function __construct($host, $username, $password, $database){
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+    
+    
 }

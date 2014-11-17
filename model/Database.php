18f5fc041698522f525de01c95337f6fd2c246af
nalitha liyanage <nalitha770@gmail.com>
2014-11-17 11:57:43 -0800

    php videos

--- model/Database.php
+++ model/Database.php
@@ -9,7 +9,10 @@ class Database {
     private $username;
     private $password;
     private $database;
-   //created a public function with $host, $username, $password, $database
+    //allows use to global variables for this function
+    //created a public function with $host, $username, $password, $database
+    //to pass any parameters on object construction then you need one.
+    //The constructor is called on an object after it has been created, and is a good place to put initialisation code, etc.
     public function __construct($host, $username, $password, $database){
         $this->host = $host;
         $this->username = $username;
